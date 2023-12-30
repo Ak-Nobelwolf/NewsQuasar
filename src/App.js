9d67397fdf96a1484adb5f3dbfe7ee7259cdd520
Ak-Nobelwolf <akshaytadakod@gmail.com>
2023-12-31 02:07:53 +0530

    Started with New Project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 
 import React, { Component } from 'react'
+import Navbar from './components/Navbar';
+import News from './components/News';
 
 export class App extends Component {
   render() {
     return (
-      <div>
-        Hello
-      </div>
+      <>
+       <Navbar/>
+       <News/>
+      </>
     )
   }
 }
